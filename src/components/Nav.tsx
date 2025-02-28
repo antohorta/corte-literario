cf6fa80d4304fa128803b71c81a781c5a64a2a75
antohorta <antonia.horta.97@gmail.com>
2025-02-28 16:30:08 -0300

    Avance search bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,22 +5,57 @@ import Container from 'react-bootstrap/Container';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import '../styles/nav.css'
 import '../styles/offcanvas.css'
-import { Link } from 'react-router-dom';
+import { Link/*, useNavigate */ } from 'react-router-dom';
 import { IGenre } from '../interfaces/IGenre';
 import { useFetch } from '../hooks/useFetch';
+import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import { IBook } from '../interfaces/IBook';
 
 const NavBar = () => {
 
+    /*     const navigate = useNavigate();
+     */
+    /* ESTADOS DE OFFCANVAS BARRA BÚSQUEDA */
+    const [show, setShow] = useState(false);
+
+    /* ESTADOS PARA BÚSQUEDA */
+    const [query, setQuery] = useState('');
+
     /* OBTENER GÉNEROS AL CARGAR COMPONENTE */
     const { data: genres, loading: loadingGenres, error: errorGenres } = useFetch<IGenre[]>('http://localhost:3000/generos');
+
+    /* OBTENER RESULTADOS BÚSQUEDA */
+    const { data: results, loading: loadingResults, error: errorResults } = useFetch<IBook[]>(`http://localhost:3000/libros?q=${query}`)
 
     /* HANDLE PARA EVITAR QUE DROPDOWN SE CIERRE */
     const handleNoPropagation = (e: React.MouseEvent) => {
         e.stopPropagation();
     };
 
-    if (loadingGenres) return <p>Cargando...</p>
-    if (errorGenres) return <p>Error en la consulta de datos {errorGenres}</p>
+    /* HANDLES DE OFFCANVAS BARRA BÚSQUEDA */
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    /* HANDLES DE OFFCANVAS BARRA BÚSQUEDA */
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value);
+    }
+
+    useEffect(() => {
+        if (!query) {
+            return;
+        }
+    }, [query]);
+
+    console.log(`La búsqueda es: ${query}`);
+    console.log(`Los resultados son: ${results}`);
+
+    if (loadingGenres) return <p>Cargando...</p>;
+    if (errorGenres) return <p>Error en la consulta de datos {errorGenres}</p>;
+
+    if (loadingResults) return <p>Cargando resultados...</p>;
+    if (errorResults) return <p>Error en la consulta de resultados {errorResults}</p>;
 
     return (
         <div className='my-nav-container'>
@@ -64,7 +99,54 @@ const NavBar = () => {
                                 </Nav>
                             </Offcanvas.Body>
                         </Navbar.Offcanvas>
-                        <Nav.Link as={Link} to={'/buscar'}>BUSCAR</Nav.Link>
+
+                        {/* BARRA DE BÚSQUEDA */}
+                        <Nav.Link as={Link} to={'#'} onClick={handleShow}>BUSCAR</Nav.Link>
+                        <Offcanvas show={show} onHide={handleClose} placement="top" className="search-nav">
+                            <Offcanvas.Body>
+                                <Offcanvas.Header closeButton></Offcanvas.Header>
+                                <Form className='search-input-container'>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Busca tu próxima historia..."
+                                            onChange={handleSearchChange}
+                                            value={query} />
+                                    </Form.Group>
+                                </Form>
+                                {/* RESULTADOS BÚSQUEDA */}
+                                <div className='results-container'>
+                                    {results && results.length > 0 && (
+                                        <div className='card-result-container'>
+                                            {results.map((book) => (
+                                                <div key={book.isbn} style={{ color: 'white' }}>
+                                                    <div className='card-result-img-container'>
+                                                        <img src="" alt="" />
+                                                    </div>
+                                                    <div className='card-result-info-container'>
+                                                        <p className='titulo'>{book.titulo}</p>
+                                                        <p className='autor'>{book.autor}</p>
+                                                        <p className='titulo'>{`$${book.precio.toLocaleString()}`}</p>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
+                                    {results && results.length === 0 && (
+                                        <p style={{ color: 'white' }}>No se encontraron resultados para "{query}.</p>
+                                    )}
+                                    <div className='all-results-container'>
+                                        <a href='#' id='all-results-link'>
+                                            Ver todos los resultados
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                <path d="M11.25 12.75V21.75H12.75V12.75H21.75V11.25H12.75V2.25L11.25 2.25V11.25H2.25V12.75H11.25Z" fill="#D9D9D9" />
+                                            </svg>
+                                        </a>
+                                    </div>
+                                </div>
+                            </Offcanvas.Body>
+                        </Offcanvas>
+
                         <Nav.Link as={Link} to={'/cuenta'}>CUENTA</Nav.Link>
                         <Nav.Link as={Link} to={'/carro'}>CARRO(0)</Nav.Link>
                     </Container>
